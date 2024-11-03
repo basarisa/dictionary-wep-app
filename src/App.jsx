17f6911b43d571b38772609fcd9701284a071c1b
Arisa Teandum <basarisa.1302@gmail.com>
2024-11-03 12:33:57 +0700

    feat: update header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-red-600">// Header</h1>
+      <Header />
       <h1>// SearchBar </h1>
       <h1>// SectionBody</h1>
+      
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 function Header() {
   return (
-    <Header>
-      <div className="logo">
-        <FontAwesomeIcon icon="fa-solid fa-book" />
+    <header>
+      <div className="flex justify-between items-center mb-8">
+        <div className="flex items-center gap-2">
+          <FontAwesomeIcon icon={faBook} size="2x" className="text-gray-500" />
+          <select
+            defaultValue=""
+            className="border-none bg-transparent font-serif text-sm"
+          >
+            <option value="">Select font</option>
+            <option value="10">Option 1</option>
+            <option value="20">Option 2</option>
+            <option value="30">Option 3</option>
+          </select>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="w-10 h-5 bg-gray-200 rounded-full relative">
+            <div className="absolute right-1 top-1 w-3 h-3 bg-purple-500 rounded-full"></div>
+          </div>
+        </div>
       </div>
-    </Header>
+    </header>
   );
 }
 
